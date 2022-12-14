f626da047dcee99bd0bd60a2a56ace06cca68fc5
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2022-12-14 10:59:01 +0700

    cau hinh

--- LTW/src/main/java/Connection/ConnectJDBC.java
+++ LTW/src/main/java/Connection/ConnectJDBC.java
@@ -9,21 +9,23 @@ public class ConnectJDBC {
 	private final String portNumber = "1433";
 	private final String instance = " ";
 	private final String userID = "sa";
-	private final String password = "12082002";
+	private final String password = "12345678";
 
-    public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
-        if (instance == null || instance.trim().isEmpty())
-            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userID, password);
-    }
+	public Connection getConnection() throws Exception {
+		String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName
+				+ ";encrypt=true;trustServerCertificate=true;";
+		if (instance == null || instance.trim().isEmpty())
+			url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName
+					+ ";encrypt=true;trustServerCertificate=true;";
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		return DriverManager.getConnection(url, userID, password);
+	}
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(new ConnectJDBC().getConnection());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			System.out.println(new ConnectJDBC().getConnection());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
