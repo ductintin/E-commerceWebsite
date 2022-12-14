2f3ac0fc0f42b3bcb2614288770d16dd34c1ad78
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2022-12-14 11:04:04 +0700

    login/signup customer

---
+++ LTW/src/main/java/Control/HomeControllerCustomer.java
@@ -0,0 +1,31 @@
+package Control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HomeControllerCustomer", value = { "/customer/home", "/home" })
+public class HomeControllerCustomer extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.getRequestDispatcher("/views/customer/home.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
---
+++ LTW/src/main/java/Control/LoginCustomer.java
@@ -0,0 +1,67 @@
+package Control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.KhachHangDAO;
+import Model.KhachHang;
+
+@WebServlet(urlPatterns = { "/customer/login" })
+public class LoginCustomer extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/views/customer/login.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+
+		KhachHangDAO KHDao = new KhachHangDAO();
+		KhachHang KH = new KhachHang();
+
+		String accountName = req.getParameter("TenTK");
+		String password = req.getParameter("MK");
+
+		String destPage = "/views/customer/login.jsp";
+		try {
+			KH = KHDao.login(accountName, password);
+
+			System.out.print(KH);
+			if (KH.equals(null) == false) {
+				HttpSession session = req.getSession();
+				session.setAttribute("user", KH);
+				destPage = "/views/customer/home.jsp";
+				resp.sendRedirect(req.getContextPath() + "/customer/home");
+			} else {
+				String msg = "Tài khoản không hợp lệ";
+				req.setAttribute("msg", msg);
+				RequestDispatcher dispatcher = req.getRequestDispatcher(destPage);
+				dispatcher.forward(req, resp);
+			}
+
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+
+	}
+}
